404c25cc07e36f6b46a5dcf0937a46ca9948ffd4
Izaak van Dongen <izaak.van.dongen@gmail.com>
2019-06-28 04:53:07 +0100

    As always, fix the links

--- screenshot_links.sh
+++ screenshot_links.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 for i in screenshots/*; do
-    echo "![screenshot](https://github.com/goedel-gang/lsystems/blob/master/screenshots/$i)"
+    echo "![screenshot](https://github.com/goedel-gang/lsystems/blob/master/$i)"
 done
